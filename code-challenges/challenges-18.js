d055e3ad98d18779f7c3a70409bc1a26aaf30a93
Mohammad-Shiyyab <mohammadshiyab1996@gmail.com>
2023-04-09 23:20:16 +0000

    18

---
+++ code-challenges/challenges-18.js
@@ -0,0 +1,86 @@
+'use strict';
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// Given a string find the length of the middle word in the sentence
+//
+// Ex:
+// Input: "You can't handle the truth!"
+// Output: 6
+//
+// Input: "You're gonna need a bigger boat"
+// Output: 1
+//
+
+const wordLength = (str) => {
+    // write your code here
+    const words = str.split(" ")
+    const middleWord = Math.ceil( (words.length - 1) / 2)
+    return words[middleWord].length;
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+//
+// Write a function that takes two strings and checks if the two strings contain the same combination of letters
+//
+// Ex:
+// Input: "dad", "add"
+// Output: true
+//
+// Input: "dad", "aadd"
+// Output: false
+//
+// Input: "table", "label"
+// Output: false
+//
+// Input: "cat", "rat"
+// Output: false
+
+const wordLetters = (str1, str2) => {
+    // write your code here
+    let comb1 = {};
+    let comb2 = {};
+
+    for (let a = 0; a < str1.length; a++) {
+        comb1[str1[a]] = comb1[str1[a]] + 1 || 1
+        comb2[str2[a]] = comb2[str2[a]] + 1 || 1
+    }
+
+    for (let key in comb1) {
+        if(comb1[key] !== comb2[key]) return false;
+    }
+    return true;
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Optional:
+//
+// Write a function that takes an integer and a sorted array
+// The function returns the index of the integer in the array
+// If the integer was not there, the function returns where it could have been
+//
+// Ex:
+// Input: [1,3,5,6], 5
+// Output: 2
+//
+// Input: [1,3,5,6], 2
+// Output: 1
+//
+// Input: [1,3,5,6], 7
+// Output: 4
+//
+
+const targetIndex = (arr, int) => {
+    // write your code here
+}
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { wordLength, wordLetters, targetIndex };
